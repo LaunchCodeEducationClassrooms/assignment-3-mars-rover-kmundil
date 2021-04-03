63d8a20245afa1009e7f5ac34056b77e9bdf0d93
kmundil <>
2021-04-03 23:14:26 +0000

    added code to rover

--- rover.js
+++ rover.js
@@ -1,5 +1,12 @@
 class Rover {
    // Write code here!
+   constructor(position, mode, generatorWatts) {
+     this.position = position;
+     this.mode = 'NORMAL'
+     this.generatorWatts = 110
+     
+
+   }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,6 +7,27 @@ const Command = require('../command.js');
 
 
 describe("Rover class", function() {
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+    let rover = new Rover
+    expect(rover.mode).toEqual('NORMAL');
+    expect(rover.generatorWatts).toEqual(110);
+  })
+
+  it("response returned by receiveMessage contains name of message", function() {
+    let message = [new Message('Test message with two commands')]
+    expect(rover.message).toEqual(message);
+  
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+
+  })  
+
+
+
+
+
+  })
+
+  
 
   // 7 tests here!
 
